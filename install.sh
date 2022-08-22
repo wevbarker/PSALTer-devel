3413ef779e603b01483a56a5662a40c9a363387f
William Barker <wb263@cam.ac.uk>
2022-08-22 01:22:41 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#	copy everything under the xAct directory into the local Mathematica install
+
+cp -r ./xAct/* ~/.Mathematica/Applications/xAct/
+
+exit 0
---
+++ xAct/Priam/Kernel/init.wl
@@ -0,0 +1,3 @@
+(* ::Package:: *)
+
+Get["xAct/Priam/Priam.m"]
