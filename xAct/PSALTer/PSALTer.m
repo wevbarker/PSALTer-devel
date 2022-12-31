b075adc567f5cc361903f6702478027f6720604c
William Barker <wb263@cam.ac.uk>
2022-12-31 22:49:31 +0000

    no recompile

--- xAct/PSALTer/PSALTer.m
+++ xAct/PSALTer/PSALTer.m
@@ -99,7 +99,7 @@ ContextList={
 };
 
 Begin["xAct`PSALTer`"];
-	xAct`PSALTer`Private`BuildPSALTer[xAct`PSALTer`Private`Recompile->True];
+	xAct`PSALTer`Private`BuildPSALTer[xAct`PSALTer`Private`Recompile->False];
 End[];
 
 End[];
