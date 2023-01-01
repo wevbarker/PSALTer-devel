e3a6c3adb3d6ebb59e27a8394cfeb78e5a053072
William Barker <wb263@cam.ac.uk>
2023-01-01 22:53:22 +0000

    more typos

--- CalibrateCase.m
+++ CalibrateCase.m
@@ -3,21 +3,28 @@
 (*=================*)
 
 CalibrateCase[CaseNumber_,CaseRules_List,CaseConditions_List]:=Module[{
+	LinearisedLagrangian,
 	Couplings={kR1,kR2,kR3,kR4,kR5,kT1,kT2,kT3,kLambda},
 	CouplingAssumptions},
 
 	Comment@("Now for a new theory. Here is the full nonlinear Lagrangian
-		for Case"<>ToString@CaseNumber<>" as defined by the second
+		for Case "<>ToString@CaseNumber<>" as defined by the second
 			column of TABLE V. in arXiv:1910.14197:");	
 
-	Print@CollectTensors@ToCanonical[NonlinearLagrangian/.#2];
+	Print@CollectTensors@ToCanonical[NonlinearLagrangian/.CaseRules];
+
+	Comment@"To use PSALTer, you have to first linearise this Lagrangian to
+	second order around the desired vacuum:";
+
+	LinearisedLagrangian=LineariseLagrangian[NonlinearLagrangian/.CaseRules];
+	Print@LineariseLagrangian;
 
 	Comment@"Now we pass this theory into the PSALTer package, which
 	computes the particle spectrum:";
 
 	ParticleSpectrum[
 		"Case"<>ToString@#1,
-		LineariseLagrangian[NonlinearLagrangian/.#2],
+		LinearisedLagrangian,
 		TensorFields->{F,A},
 		CouplingConstants->Couplings,
 		ExportTheory->True
