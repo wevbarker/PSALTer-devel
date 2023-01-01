91481c282d0d5a217974d9b410b59970aff3ba5f
William Barker <wb263@cam.ac.uk>
2023-01-01 23:02:16 +0000

    some remaining formatting

--- CalibrateCase.m
+++ CalibrateCase.m
@@ -7,34 +7,26 @@ CalibrateCase[CaseNumber_,CaseRules_List,CaseConditions_List]:=Module[{
 	Couplings={kR1,kR2,kR3,kR4,kR5,kT1,kT2,kT3,kLambda},
 	CouplingAssumptions},
 
-	Comment@("Now for a new theory. Here is the full nonlinear Lagrangian
-		for Case "<>ToString@CaseNumber<>" as defined by the second
-			column of TABLE V. in arXiv:1910.14197:");	
+	Comment@("Now for a new theory. Here is the full nonlinear Lagrangian for Case "<>ToString@CaseNumber<>" as defined by the second column of TABLE V. in arXiv:1910.14197:");	
 
 	Print@CollectTensors@ToCanonical[NonlinearLagrangian/.CaseRules];
 
-	Comment@"To use PSALTer, you have to first linearise this Lagrangian to
-	second order around the desired vacuum:";
+	Comment@"To use PSALTer, you have to first linearise this Lagrangian to second order around the desired vacuum:";
 
 	LinearisedLagrangian=LineariseLagrangian[NonlinearLagrangian/.CaseRules];
-	Print@LineariseLagrangian;
+	Print@LinearisedLagrangian;
 
-	Comment@"Now we pass this theory into the PSALTer package, which
-	computes the particle spectrum:";
+	Comment@"Now we pass this theory into the PSALTer package, which computes the particle spectrum:";
 
 	ParticleSpectrum[
-		"Case"<>ToString@#1,
+		"Case"<>ToString@CaseNumber,
 		LinearisedLagrangian,
 		TensorFields->{F,A},
 		CouplingConstants->Couplings,
 		ExportTheory->True
 	];
 
-	Comment@"You can check the particle content against TABLE IV. in
-	arXiv:1910.14197. If you take the overall unitarity conditions from the
-	final column in TABLE V., and decompose them using Mathematica's Reduce
-		function, you get the following (to be compared with the
-			PSALTer conditions above):";
+	Comment@"So, that's the end of the PSALTer output for this theory. You can check the particle content against TABLE IV. in arXiv:1910.14197. If you take the overall unitarity conditions from the final column in TABLE V., and decompose them using Mathematica's Reduce function, you get the following (to be compared with the PSALTer conditions above):";
 	
 	CouplingAssumptions=(#~Element~Reals)&/@Couplings;
 	Print@Assuming[CouplingAssumptions,Reduce[CaseConditions,Couplings]];
--- Run.m
+++ Run.m
@@ -6,9 +6,7 @@ Print@"First we import some formatting...";
 
 Get@FileNameJoin@{NotebookDirectory[],"VimFormat.m"};
 
-Comment@"Okay, that's better, from now on any commentary written inside this
-Calibration.m wrapper will present as blue text (i.e. this text is not part of
-	PSALTer, it is just a use-case). Next we load the PSALTer package:";
+Comment@"...okay, that's better, from now on any commentary written inside this Calibration.m wrapper will present as blue text (i.e. this text is not part of PSALTer, it is just a use-case). Next we load the PSALTer package:";
 
 <<xAct`PSALTer`;
 
@@ -23,9 +21,6 @@ Comment@"We also knock up some simple tools to linearise the Lagrangian:";
 
 Get@FileNameJoin@{NotebookDirectory[],"Linearise.m"};
 
-Comment@"Next we write down all the cases which define the theories in
-arXiv:1910.14197.";
-
 Get@FileNameJoin@{NotebookDirectory[],"CriticalCases.m"};
 Get@FileNameJoin@{NotebookDirectory[],"Unitarity.m"};
 
@@ -33,7 +28,7 @@ Off[Solve::svars];
 CriticalCasesSolutions=First/@(Solve[#,{kR1,kR2,kR3,kR4,kR5,kT1,kT2,kT3,kLambda}]&/@CriticalCases);
 On[Solve::svars];
 
-Comment@"Now to perform the calibration";
+Comment@"We are now ready to check that PSALTer is getting the physics right by running it on the 58 theories in arXiv:1910.14197.";
 
 (*
 ParticleSpectrum[
@@ -51,13 +46,12 @@ Get@FileNameJoin@{NotebookDirectory[],"CalibrateCase.m"};
 CalibrationTimingData=MapThread[
 		AbsoluteTiming@CalibrateCase[#1,#2,#3]&,
 		{
-			Table[i,{i,1,2}],
-			CriticalCasesSolutions[[1;;2]],
-			Unitarity[[1;;2]]
+			Table[i,{i,1,58}],
+			CriticalCasesSolutions[[1;;58]],
+			Unitarity[[1;;58]]
 		}];
 
-Comment@"Okay, that's all the cases. You can see from the timing below (in
-	seconds) that each theory takes about a minute to process:";
+Comment@"Okay, that's all the cases. You can see from the timing below (in seconds) that each theory takes about a minute to process:";
 
 Print@CalibrationTimingData;
 
