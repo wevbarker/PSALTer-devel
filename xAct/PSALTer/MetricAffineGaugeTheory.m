37dc2ec0d8459c55f05a91b9124246f739eb4ddb
William Barker <wb263@cam.ac.uk>
2023-06-17 16:05:08 +0100

    strip

--- xAct/PSALTer/MetricAffineGaugeTheory.m
+++
@@ -1,6 +0,0 @@
-(*===========================*)
-(*  MetricAffineGaugeTheory  *)
-(*===========================*)
-
-BeginPackage["xAct`PSALTer`MetricAffineGaugeTheory`",{"xAct`xTensor`","xAct`xPerm`","xAct`xCore`","xAct`xTras`","xAct`xCoba`","xAct`PSALTer`"}];
-EndPackage[];
