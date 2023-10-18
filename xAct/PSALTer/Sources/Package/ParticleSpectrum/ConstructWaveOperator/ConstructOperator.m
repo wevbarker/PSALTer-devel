31c56eb3b21cfabe994ca8c6c6c0860d641931c1
William Barker <wb263@cam.ac.uk>
2023-10-19 00:26:11 +0100

    with diagnostic

--- xAct/PSALTer/Sources/Package/ParticleSpectrum/ConstructWaveOperator/ConstructOperator.m
+++ xAct/PSALTer/Sources/Package/ParticleSpectrum/ConstructWaveOperator/ConstructOperator.m
@@ -24,6 +24,7 @@ ConstructOperator[ClassName_?StringQ,Expr_,Couplings_]:=Module[{
 	
 	CouplingAssumptions=(#~Element~Reals)&/@Couplings;
 	Diagnostic@CouplingAssumptions;
+	Throw["h"];
 	CouplingAssumptions~AppendTo~(xAct`PSALTer`Def~Element~Reals);
 
 	Class=Evaluate@Symbol@ClassName;
