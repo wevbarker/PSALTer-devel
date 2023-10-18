2d8d607892b52785ba54d530e89eb4096d34abb3
William Barker <wb263@cam.ac.uk>
2023-10-19 00:51:08 +0100

    with diagnostic

--- xAct/PSALTer/Documentation/English/Calibration/MetricAffineGravity/IosifidisBarkerTheory.m
+++ xAct/PSALTer/Documentation/English/Calibration/MetricAffineGravity/IosifidisBarkerTheory.m
@@ -36,4 +36,4 @@ TestTheory[InputRules_]:=Module[{NewLinearLagrangian},
 			MaxLaurentDepth->1];	
 ];
 
-TestTheory@{A1->0,A2->0,A3->0,A4->0,A5->0,A6->0,A7->0,A8->0,A9->0,A10->0,A11->0,C1->0,C2->0,C3->0,C4->0,C5->0,C6->0,C7->0,C8->0,C9->0,C10->0,C11->0,C12->0,C13->0,C14->0,C15->0,C16->0};
+TestTheory@{A0->xAct`PSALTer`MetricAffineGravity`A0,A1->0,A2->0,A3->0,A4->0,A5->0,A6->0,A7->0,A8->0,A9->0,A10->0,A11->0,C1->0,C2->0,C3->0,C4->0,C5->0,C6->0,C7->0,C8->0,C9->0,C10->0,C11->0,C12->0,C13->0,C14->0,C15->0,C16->0};
