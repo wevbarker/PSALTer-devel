752a67662e6c3d40e74f84b565508f23914f1011
William Barker <wb263@cam.ac.uk>
2023-10-19 19:09:35 +0100

    asking for timings

--- xAct/PSALTer/Sources/Package/ParticleSpectrum/ConstructMasslessAnalysis/ConvertLightcone/ExpressInLightcone.m
+++ xAct/PSALTer/Sources/Package/ParticleSpectrum/ConstructMasslessAnalysis/ConvertLightcone/ExpressInLightcone.m
@@ -49,5 +49,6 @@ ExpressInLightcone[ClassName_?StringQ,RawSector_,SourceComponentsToFreeSourceVar
 	Sector//=Expand;
 
 	Sector=(If[Head@#===Plus,List@@#,List@#])&@(Sector);
+	Sector//=(#~PadRight~{Length@#,First@((Length/@#)~TakeLargest~1)})&;
 
 Sector];
