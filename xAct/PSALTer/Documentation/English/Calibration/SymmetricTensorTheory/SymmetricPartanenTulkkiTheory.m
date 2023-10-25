a15b83bab9d13c2a7383f26d7a12cfdd025aad60
William Barker <wb263@cam.ac.uk>
2023-10-25 20:09:01 +0100

    with edited symbols

--- xAct/PSALTer/Documentation/English/Calibration/SymmetricTensorTheory/SymmetricPartanenTulkkiTheory.m
+++ xAct/PSALTer/Documentation/English/Calibration/SymmetricTensorTheory/SymmetricPartanenTulkkiTheory.m
@@ -6,8 +6,8 @@ Section@"Partanen-Tulkki gravity";
 
 Comment@"We now examine the theory proposed in arXiv:2310.01460.";
 LinearisedLagrangian=-(1/(8*xAct`PSALTer`SymmetricTensorTheory`EinsteinConstant))*(
-	(CD[-m]@xAct`PSALTer`SymmetricTensorTheory`LinearMetric[-r,-n]-CD[-n]@xAct`PSALTer`SymmetricTensorTheory`LinearMetric[-r,-m])*
-	(CD[m]@xAct`PSALTer`SymmetricTensorTheory`LinearMetric[r,n]-CD[n]@xAct`PSALTer`SymmetricTensorTheory`LinearMetric[r,m])
+	(CD[-m]@xAct`PSALTer`SymmetricTensorTheory`SymmetricLinearMetric[-r,-n]-CD[-n]@xAct`PSALTer`SymmetricTensorTheory`SymmetricLinearMetric[-r,-m])*
+	(CD[m]@xAct`PSALTer`SymmetricTensorTheory`SymmetricLinearMetric[r,n]-CD[n]@xAct`PSALTer`SymmetricTensorTheory`SymmetricLinearMetric[r,m])
 );
 DisplayExpression[LinearisedLagrangian,EqnLabel->"SymmetricPartanenTulkkiTheory"];
 LinearisedLagrangian//=ToCanonical;
