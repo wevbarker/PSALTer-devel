df9fb6d57f97e79ff222ef5fb89237ba209a1656
William Barker <wb263@cam.ac.uk>
2024-01-07 05:44:17 +0000

    transfer to system orange

--- xAct/PSALTer/Documentation/English/Calibration/WeylGaugeTheoryExtended/WGTEGeneralCase.m
+++ xAct/PSALTer/Documentation/English/Calibration/WeylGaugeTheoryExtended/WGTEGeneralCase.m
@@ -7,6 +7,7 @@
 (*=====================*)
 (*  Most general WGT  *)
 (*=====================*)
+(*
 Section@"Most general eWGT";
 Comment@{"We will study the most general eWGT as defined in eqn 13 of Lin's draft paper."};
 
@@ -24,7 +25,30 @@ ParticleSpectrum[
 	ClassName->"WeylGaugeTheory",
 	TheoryName->"GeneralWGTE",	
 	Method->"Hard",
-	MaxLaurentDepth->1
+	MaxLaurentDepth->3
 ];
 
 Supercomment@"This marks the completion of the particle spectrum analysis for the general WGT."
+*)
+
+Section@"Killing off the quartic pole";
+Comment@{"We will kill the quartic pole."};
+
+DisplayExpression@CollectTensors@ToCanonical[NonlinearLagrangianWGTE/.{lXi->0}];
+LinearisedLagrangianWGTE=LineariseLagrangianWGTE[NonlinearLagrangianWGTE];
+
+Comment@{"Here is the linearised Lagrangian before feeding into ParticleSpectrum[]."};
+DisplayExpression@CollectTensors@ToCanonical[LinearisedLagrangianWGTE];
+
+(*Diagnostic line*)
+Print@"Hi there, I'm sitting between Zhiyuan's code and Will's code! Method -> easy mode.";
+
+ParticleSpectrum[
+	LinearisedLagrangianWGTE,
+	ClassName->"WeylGaugeTheory",
+	TheoryName->"NoQuarticWGTE",	
+	Method->"Hard",
+	MaxLaurentDepth->3
+];
+
+Supercomment@"This marks the completion of the particle spectrum analysis for the removed quartic pole."
