d0ddf26dec70268e53560f8030efb9551c025aea
William Barker <wb263@cam.ac.uk>
2024-01-19 14:30:40 +0000

    prepare to merge into master

--- xAct/PSALTer/Documentation/English/CalibrationWGTE.m
+++ xAct/PSALTer/Documentation/English/CalibrationWGTE.m
@@ -29,7 +29,7 @@ Comment@"The first step is to load the PSALTer package.";
 <<xAct`PSALTer`;
 
 (*For HPC*)
-
+(*
 $NodeCPU=110;
 Comment@{"The variable \$NodeCPU is set to ",ToString@$NodeCPU};
 Comment@{"The variable \$ProcessorCount is set to ",ToString@$ProcessorCount};
@@ -40,6 +40,7 @@ Comment@{"The variable \$KernelCount is set to ",ToString@$KernelCount};
 Comment@{"Now we execute LaunchKernels[]"};
 LaunchKernels@$NodeCPU;
 Comment@{"The variable \$KernelCount is set to ",ToString@$KernelCount};
+*)
 
 
 (*Global settings for xAct*)
