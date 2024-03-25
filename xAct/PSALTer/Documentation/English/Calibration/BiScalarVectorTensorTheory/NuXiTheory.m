ebe6638c5014c3ae420abbbff4e8a961c50cf648
William Barker <wb263@cam.ac.uk>
2024-03-25 08:53:58 +0000

    functional setup for bi-scalar-vector-tensor

--- xAct/PSALTer/Documentation/English/Calibration/BiScalarVectorTensorTheory/NuXiTheory.m
+++ xAct/PSALTer/Documentation/English/Calibration/BiScalarVectorTensorTheory/NuXiTheory.m
@@ -13,6 +13,7 @@ LinearisedLagrangian=Sqrt[-NonlinearMeasure[]]*(
 +(1/2)*NonlinearMetric[a,b]*xAct`PSALTer`BiScalarVectorTensorTheory`CouplingNu*(CD[-a]@xAct`PSALTer`BiScalarVectorTensorTheory`Phi[]-xAct`PSALTer`BiScalarVectorTensorTheory`Phi[]*xAct`PSALTer`BiScalarVectorTensorTheory`B[-a])*(CD[-b]@xAct`PSALTer`BiScalarVectorTensorTheory`Phi[]-xAct`PSALTer`BiScalarVectorTensorTheory`Phi[]*xAct`PSALTer`BiScalarVectorTensorTheory`B[-b])
 -(1/2)*xAct`PSALTer`BiScalarVectorTensorTheory`CouplingMu^2*xAct`PSALTer`BiScalarVectorTensorTheory`Phi[]^2*xAct`PSALTer`BiScalarVectorTensorTheory`VarPhi[]^2
 );
+
 DisplayExpression[LinearisedLagrangian,EqnLabel->"NuXiTheory"];
 DefConstantSymbol[PerturbativeParameter,PrintAs->"\[Epsilon]"];
 NonlinearMetricRule=MakeRule[{NonlinearMetric[a,b],G[a,b]-PerturbativeParameter*xAct`PSALTer`BiScalarVectorTensorTheory`LinearMetric[a,b]},MetricOn->All,ContractMetrics->True];
@@ -20,17 +21,27 @@ NonlinearMeasureRule=MakeRule[{NonlinearMeasure[],-1+PerturbativeParameter*xAct`
 
 TotalRules=Join[NonlinearMetricRule,NonlinearMeasureRule,{
 xAct`PSALTer`BiScalarVectorTensorTheory`Phi[]->xAct`PSALTer`BiScalarVectorTensorTheory`CouplingPhi0+PerturbativeParameter*xAct`PSALTer`BiScalarVectorTensorTheory`Phi[],
-xAct`PSALTer`BiScalarVectorTensorTheory`VarPhi[]->xAct`PSALTer`BiScalarVectorTensorTheory`CouplingVarPhi0+PerturbativeParameter*xAct`PSALTer`BiScalarVectorTensorTheory`VarPhi[],
+xAct`PSALTer`BiScalarVectorTensorTheory`VarPhi[]->PerturbativeParameter*xAct`PSALTer`BiScalarVectorTensorTheory`VarPhi[],
+(*xAct`PSALTer`BiScalarVectorTensorTheory`VarPhi[]->xAct`PSALTer`BiScalarVectorTensorTheory`CouplingVarPhi0+PerturbativeParameter*xAct`PSALTer`BiScalarVectorTensorTheory`VarPhi[],*)
 xAct`PSALTer`BiScalarVectorTensorTheory`B[Anything___]->PerturbativeParameter*xAct`PSALTer`BiScalarVectorTensorTheory`B[Anything]
 }];
 LinearisedLagrangian=LinearisedLagrangian/.TotalRules;
-DisplayExpression[LinearisedLagrangian,EqnLabel->"NuXiTheory"];
+
 LinearisedLagrangian//=Series[#,{PerturbativeParameter,0,2}]&;
 LinearisedLagrangian//=Normal;
 LinearisedLagrangian=LinearisedLagrangian/.PerturbativeParameter->1;
 LinearisedLagrangian//=ToCanonical;
 LinearisedLagrangian//=CollectTensors;
+
 DisplayExpression[LinearisedLagrangian,EqnLabel->"NuXiTheory"];
+(*
+LinearisedLagrangian=LinearisedLagrangian/.{xAct`PSALTer`BiScalarVectorTensorTheory`CouplingMu->1,xAct`PSALTer`BiScalarVectorTensorTheory`CouplingNu->1,xAct`PSALTer`BiScalarVectorTensorTheory`CouplingVarPhi0->0};
+*)
+LinearisedLagrangian//=ToCanonical;
+LinearisedLagrangian//=CollectTensors;
+
+DisplayExpression[LinearisedLagrangian,EqnLabel->"NuXiTheory"];
+
 ParticleSpectrum[
 	LinearisedLagrangian,
 	ClassName->"BiScalarVectorTensorTheory",
