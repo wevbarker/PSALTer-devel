0657878f083677408de33948884992ac8d9ea9c5
zw351 <zw351@cam.ac.uk>
2024-04-02 01:09:29 +0200

    Zhiyuan: updated the code to run the new theory classes. Note that at the moment the code does not run; there is an issue in the PSALTer file.

---
+++ xAct/PSALTer/Classes/WeylEinsteinGaugeTheory.m
@@ -0,0 +1,8 @@
+(* ::Package:: *)
+
+(*===========================*)
+(*  WeylEinsteinGaugeTheory  *)
+(*===========================*)
+
+BeginPackage["xAct`PSALTer`WeylEinsteinGaugeTheory`",{"xAct`xTensor`","xAct`xPerm`","xAct`xCore`","xAct`xTras`","xAct`xCoba`","xAct`PSALTer`"}];
+EndPackage[];
---
+++ xAct/PSALTer/Classes/WeylSIVGaugeTheory.m
@@ -0,0 +1,8 @@
+(* ::Package:: *)
+
+(*======================*)
+(*  WeylSIVGaugeTheory  *)
+(*======================*)
+
+BeginPackage["xAct`PSALTer`WeylSIVGaugeTheory`",{"xAct`xTensor`","xAct`xPerm`","xAct`xCore`","xAct`xTras`","xAct`xCoba`","xAct`PSALTer`"}];
+EndPackage[];
