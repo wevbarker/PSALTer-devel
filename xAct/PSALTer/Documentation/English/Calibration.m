846ec2166427da2eea7a0c2913591ce49284a490
William Barker <wb263@cam.ac.uk>
2024-04-08 00:35:56 +0100

    with an ongoing run for all paper-e results

--- xAct/PSALTer/Documentation/English/Calibration.m
+++ xAct/PSALTer/Documentation/English/Calibration.m
@@ -58,7 +58,7 @@ Protect@AutomaticRules;
 $DiagnosticMode=False;
 $MonitorParallel=False;
 $ExportPDF=False;
-$ReadOnly=True;
+$ReadOnly=False;
 
 Comment@"Great, so PSALTer is now loaded and we can start to do some science.";
 
--- xAct/PSALTer/Documentation/English/Calibration/PoincareGaugeTheory/GeneraleWGT.m
+++ xAct/PSALTer/Documentation/English/Calibration/PoincareGaugeTheory/GeneraleWGT.m
@@ -3,7 +3,8 @@
 (*===============*)
 
 Section@"Most general eWGT";
-(*
+
+Comment@"First we set up the non-linear Lagrangian.";
 NewNonlinearLagrangian=(
 	-(1/2)*MPl0*R[i,j,-i,-j]+
 	T[-m,-n,-s]*(
@@ -24,118 +25,64 @@ NewNonlinearLagrangian=(
 	(1/3)*Chi0*R[i,-m,-i,-n]*(1/2)*(CD[m]@T[j,-j,n]-CD[n]@T[j,-j,m])+
 	(1/36)*Xi0*(1/2)*(CD[-m]@T[k,-k,-n]-CD[-n]@T[k,-k,-m])*(1/2)*(CD[m]@T[j,-j,n]-CD[n]@T[j,-j,m])
 );
-
 DisplayExpression[NewNonlinearLagrangian];
 
-(*NewNonlinearLagrangian=NewNonlinearLagrangian/.{Xi0->Chi0^2/(2*Alp2+4*Alp4+Alp5)};*)
-
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Alp5->kT3*Chi0-(2*Alp2+4*Alp4)};
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Xi0->Chi0/kT3};
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Chi0->kT3*kT1};
-
-NewNonlinearLagrangian//=ToCanonical;
-NewNonlinearLagrangian//=ContractMetric;
-NewNonlinearLagrangian//=ScreenDollarIndices;
-NewNonlinearLagrangian//=CollectTensors;
-
-DisplayExpression[NewNonlinearLagrangian];
-
-Comment@{"We want to study the most general eWGT."};
-DisplayExpression@CollectTensors@ToCanonical[NewNonlinearLagrangian];
-LinearLagrangian=LineariseLagrangian[NewNonlinearLagrangian];
-DisplayExpression@LinearLagrangian;
-*)
-(*
+Comment@"We first try the PGT equivalent.";
+SpecialNonlinearLagrangian=NewNonlinearLagrangian/.{Chi0->0,Xi->0};
+SpecialNonlinearLagrangian//=LineariseLagrangian;
+SpecialNonlinearLagrangian//DisplayExpression;
 ParticleSpectrum[
-	LinearLagrangian,
+	SpecialNonlinearLagrangian,
+	ClassName->"PoincareGaugeTheory",
+	TheoryName->"PGTeWGT",	
+	Method->"Hard",
+	MaxLaurentDepth->3
+];
+
+Comment@"We next try the discontinuous PGT equivalent.";
+SpecialNonlinearLagrangian=NewNonlinearLagrangian/.{Alp5->-(2*Alp2+4*Alp4)};
+SpecialNonlinearLagrangian=SpecialNonlinearLagrangian/.{Chi0->0,Xi->0};
+SpecialNonlinearLagrangian//=LineariseLagrangian;
+SpecialNonlinearLagrangian//DisplayExpression;
+ParticleSpectrum[
+	SpecialNonlinearLagrangian,
+	ClassName->"PoincareGaugeTheory",
+	TheoryName->"DiscontinuousPGTeWGT",	
+	Method->"Hard",
+	MaxLaurentDepth->3
+];
+
+Comment@"Now we impose the basic conditions.";
+NewNonlinearLagrangian=NewNonlinearLagrangian/.{Alp5->Chi0^2/Xi0-(2*Alp2+4*Alp4)};
+NewNonlinearLagrangian=NewNonlinearLagrangian/.{Chi0->kT1*Xi0};
+NewNonlinearLagrangian//=CollectTensors;
+DisplayExpression[NewNonlinearLagrangian];
+
+Comment@"We first try the root theory.";
+SpecialNonlinearLagrangian=NewNonlinearLagrangian;
+SpecialNonlinearLagrangian//=LineariseLagrangian;
+SpecialNonlinearLagrangian//DisplayExpression;
+ParticleSpectrum[
+	SpecialNonlinearLagrangian,
 	ClassName->"PoincareGaugeTheory",
 	TheoryName->"GeneraleWGT",	
 	Method->"Hard",
 	MaxLaurentDepth->3
 ];
-Comment@"Some comments to be made.";
 
-NewNonlinearLagrangian=(
-	-(1/2)*MPl0*R[i,j,-i,-j]+
-	T[-m,-n,-s]*(
-		Bet1*+T[m,n,s]+
-		Bet2*+T[n,m,s]
-	)+
-	Bet3*T[i,-i,s]*T[j,-j,-s]+
-	Alp1*R[i,j,-i,-j]*R[k,l,-k,-l]+
-	R[i,-m,-i,-n]*(
-		Alp2*R[j,m,-j,n]+
-		Alp3*R[j,n,-j,m]
-	)+
-	+R[-m,-n,-s,-l]*(
-		Alp4*R[m,n,s,l]+
-		Alp5*R[m,s,n,l]+
-		Alp6*R[s,l,m,n]
-	)+
-	(*(1/3)*Chi0*R[i,-m,-i,-n]*(1/2)*(CD[m]@T[j,-j,n]-CD[n]@T[j,-j,m])+*)
-	(1/36)*Xi0*(1/2)*(CD[-m]@T[k,-k,-n]-CD[-n]@T[k,-k,-m])*(1/2)*(CD[m]@T[j,-j,n]-CD[n]@T[j,-j,m])
-);
-
-DisplayExpression[NewNonlinearLagrangian];
-
-(*NewNonlinearLagrangian=NewNonlinearLagrangian/.{Xi0->Chi0^2/(2*Alp2+4*Alp4+Alp5)};*)
-(*
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Xi0->Chi0/kT3};
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Chi0->kT3*kT1};
-*)
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Alp5->-(2*Alp2+4*Alp4)};
-NewNonlinearLagrangian//=ToCanonical;
-NewNonlinearLagrangian//=ContractMetric;
-NewNonlinearLagrangian//=ScreenDollarIndices;
-NewNonlinearLagrangian//=CollectTensors;
-
-DisplayExpression[NewNonlinearLagrangian];
-
-Comment@{"We want to study the most general eWGT."};
-DisplayExpression@CollectTensors@ToCanonical[NewNonlinearLagrangian];
-LinearLagrangian=LineariseLagrangian[NewNonlinearLagrangian];
-DisplayExpression@LinearLagrangian;
+Comment@"We next try the Maxwell limit.";
+SpecialNonlinearLagrangian=NewNonlinearLagrangian/.{kT1->0};
+SpecialNonlinearLagrangian//=LineariseLagrangian;
+SpecialNonlinearLagrangian//DisplayExpression;
 ParticleSpectrum[
-	LinearLagrangian,
+	SpecialNonlinearLagrangian,
 	ClassName->"PoincareGaugeTheory",
 	TheoryName->"NoChieWGT",	
 	Method->"Hard",
 	MaxLaurentDepth->3
 ];
-Comment@"Some comments to be made.";
-*)
-
-NewNonlinearLagrangian=(
-	-(1/2)*MPl0*R[i,j,-i,-j]+
-	T[-m,-n,-s]*(
-		Bet1*+T[m,n,s]+
-		Bet2*+T[n,m,s]
-	)+
-	Bet3*T[i,-i,s]*T[j,-j,-s]+
-	Alp1*R[i,j,-i,-j]*R[k,l,-k,-l]+
-	R[i,-m,-i,-n]*(
-		Alp2*R[j,m,-j,n]+
-		Alp3*R[j,n,-j,m]
-	)+
-	+R[-m,-n,-s,-l]*(
-		Alp4*R[m,n,s,l]+
-		Alp5*R[m,s,n,l]+
-		Alp6*R[s,l,m,n]
-	)+
-	(1/3)*Chi0*R[i,-m,-i,-n]*(1/2)*(CD[m]@T[j,-j,n]-CD[n]@T[j,-j,m])+
-	(1/36)*Xi0*(1/2)*(CD[-m]@T[k,-k,-n]-CD[-n]@T[k,-k,-m])*(1/2)*(CD[m]@T[j,-j,n]-CD[n]@T[j,-j,m])
-);
-
-DisplayExpression[NewNonlinearLagrangian];
-
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Alp5->kT3*Chi0-(2*Alp2+4*Alp4)};
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Xi0->Chi0/kT3};
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Chi0->kT3*kT1};
-
-DisplayExpression[NewNonlinearLagrangian];
 
 Comment@"Now we will try to look for new solutions to the vector system.";
-
 NewTheoryConditions={	
 	(2Bet1+Bet2+3Bet3)(6Alp1+2Alp3-2Alp4+2Alp6+Chi0^2/Xi0)==0,
 	4Alp2+12Alp4-2Chi0^2/Xi0==0,
@@ -148,27 +95,33 @@ Comment@"From the general mass parameters, here are the conditions which must be
 DisplayExpression/@NewTheoryConditions;
 Comment@"Now we obtain the following solutions.";
 NewTheoryRules=Solve@NewTheoryConditions;
-NewTheoryRules=NewTheoryRules/.{Chi0^2->Xi0*kT1*kT3^2};
+NewTheoryRules=NewTheoryRules/.{Chi0->kT1*Xi0};
 NewTheoryRules//=FullSimplify;
 DisplayExpression/@NewTheoryRules;
 
 TryTheory[TheTheory_]:=Module[{
 		NewTheoryName="TrialeWGT"<>ToString@TheTheory,
 		NewTheoryRule=NewTheoryRules[[TheTheory]],
-		NewestNonlinearLagrangian
+		NewestNonlinearLagrangian,
+		SuperRules
 		},
 
-	Comment@"Trying a new theory.";
+	Subsection@{"Trying a new theory with name",NewTheoryName," and the following replacement."};
 	DisplayExpression@NewTheoryRule;
-	DisplayExpression@NewTheoryName;
+	Comment@"This leads to the following overall definition.";
+	SuperRules=NewTheoryRule/.{kT1->Chi0/Xi0};
+	SuperRules={Alp5->Chi0^2/Xi0-(2*Alp2+4*Alp4)}~Join~SuperRules;
+	SuperRules=SuperRules/.{Rule->Equal};
+	SuperRules//=Reduce;
+	DisplayExpression@SuperRules;
+	Comment@"This leads to the following non-linear Lagrangian.";
 	NewestNonlinearLagrangian=NewNonlinearLagrangian/.NewTheoryRule;
-(*NewestNonlinearLagrangian=NewestNonlinearLagrangian/.{Bet1->0,Bet3->0};*)
-	DisplayExpression@NewestNonlinearLagrangian;
 	NewestNonlinearLagrangian//=ToCanonical;
 	NewestNonlinearLagrangian//=ContractMetric;
 	NewestNonlinearLagrangian//=ScreenDollarIndices;
 	NewestNonlinearLagrangian//=CollectTensors;
 	NewestNonlinearLagrangian//DisplayExpression;
+	Comment@"This leads to the following linear Lagrangian.";
 	NewestNonlinearLagrangian//=LineariseLagrangian;
 	NewestNonlinearLagrangian//DisplayExpression;
 	ParticleSpectrum[
@@ -181,31 +134,4 @@ TryTheory[TheTheory_]:=Module[{
 	Comment@"Some comments to be made.";
 ];
 
-TryTheory/@Table[i,{i,1,6}];
-
-
-
-
-(*
-NewNonlinearLagrangian=NewNonlinearLagrangian/.{Alp5->-(2*Alp2+4*Alp4)};
-NewNonlinearLagrangian//=ToCanonical;
-NewNonlinearLagrangian//=ContractMetric;
-NewNonlinearLagrangian//=ScreenDollarIndices;
-NewNonlinearLagrangian//=CollectTensors;
-
-DisplayExpression[NewNonlinearLagrangian];
-
-Comment@{"We want to study the most general eWGT."};
-DisplayExpression@CollectTensors@ToCanonical[NewNonlinearLagrangian];
-LinearLagrangian=LineariseLagrangian[NewNonlinearLagrangian];
-DisplayExpression@LinearLagrangian;
-ParticleSpectrum[
-	LinearLagrangian,
-	ClassName->"PoincareGaugeTheory",
-	TheoryName->"DiscontinuousPGTeWGT",	
-	(*TheoryName->"PGTeWGT",	*)
-	Method->"Hard",
-	MaxLaurentDepth->3
-];
-Comment@"Some comments to be made.";
-*)
+TryTheory/@Table[i,{i,1,2}];
