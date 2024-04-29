f583c415ee73f2c2b482337dd82cfd81cf865641
William Barker <wb263@cam.ac.uk>
2024-04-29 20:52:05 +0100

    tidied repo

--- Test.m
+++ Test.m
@@ -2,13 +2,6 @@
 
 Comment@"Now we will try to look for new solutions to the vector system.";
 
-CellPrint@ExpressionCell@NewVar;
-
-ClearAll[gencode];
-SetAttributes[gencode, HoldAll];
-gencode[code_, opts : OptionsPattern[Cell]] :=
- CellPrint@ExpressionCell[Defer@code, "Input", opts];
-
-gencode[Total[{2,3}]];
+Code[Total@{1,3,54}];
 
 Quit[];
