1bd57994719cafe5bb820243740dbeae0dd51edd
William Barker <wb263@cam.ac.uk>
2024-05-13 09:44:29 +0100

    tentative implementation of NLC

--- xAct/PSALTer/Documentation/English/Documentation/PoincareGaugeTheory/TeleparallelTheory.m
+++ xAct/PSALTer/Documentation/English/Documentation/PoincareGaugeTheory/TeleparallelTheory.m
@@ -3,6 +3,122 @@
 (*======================*)
 
 Subsection@"Teleparallel Theory";
+
+Quiet[
+	DefConstantSymbol[Prt,PrintAs->"\[Epsilon]"];
+	DefConstantSymbol[C1,PrintAs->"\*SubscriptBox[\[ScriptC],1]"];
+	DefConstantSymbol[C2,PrintAs->"\*SubscriptBox[\[ScriptC],2]"];
+	DefConstantSymbol[Gam,PrintAs->"\[Gamma]"];
+	DefConstantSymbol[JBet0,PrintAs->"\*SubscriptBox[\[Beta],0]"];
+	DefConstantSymbol[JBet1,PrintAs->"\*SubscriptBox[\[Beta],1]"];
+	DefConstantSymbol[JBet2,PrintAs->"\*SubscriptBox[\[Beta],2]"];
+	DefConstantSymbol[JBet3,PrintAs->"\*SubscriptBox[\[Beta],3]"];
+	DefConstantSymbol[JBet4,PrintAs->"\*SubscriptBox[\[Beta],4]"];
+];
+
+DefTensor[CetE[i,-m],M4,PrintAs->"\[GothicE]"];
+DefTensor[CetF[i,-m],M4,PrintAs->"\[GothicF]"];
+DefTensor[TetE[-i,m],M4,PrintAs->"\[ScriptE]"];
+DefTensor[TetF[-i,m],M4,PrintAs->"\[ScriptF]"];
+
+Code[
+	DefField[SymE[-m,-n],Symmetric[{-m,-n}],PrintAs->"\*OverscriptBox[\[ScriptH],\[GothicE]]",PrintSourceAs->"\*OverscriptBox[\[Tau],\[GothicE]]"];
+	DefField[AntE[-m,-n],Antisymmetric[{-m,-n}],PrintAs->"\*OverscriptBox[\[ScriptB],\[GothicE]]",PrintSourceAs->"\*OverscriptBox[\[ScriptJ],\[GothicE]]"];
+	DefField[SymF[-m,-n],Symmetric[{-m,-n}],PrintAs->"\*OverscriptBox[\[ScriptH],\[GothicF]]",PrintSourceAs->"\*OverscriptBox[\[Tau],\[GothicF]]"];
+	DefField[AntF[-m,-n],Antisymmetric[{-m,-n}],PrintAs->"\*OverscriptBox[\[ScriptB],\[GothicF]]",PrintSourceAs->"\*OverscriptBox[\[ScriptJ],\[GothicF]]"];
+];
+
+DefTensor[TE[i,-m,-n],M4,Antisymmetric[{-m,-n}],PrintAs->"\*OverscriptBox[\[ScriptT],\[GothicE]]"];
+
+ExpandTE=MakeRule[{TE[i,-m,-n],CD[-m]@CetE[i,-n]-CD[-n]@CetE[i,-m]},MetricOn->All,ContractMetrics->True];
+ExpandCetE=MakeRule[{CetE[i,-m],G[i,-m]+(1/2)*Prt*G[i,j]*G[n,-j]*(SymE[-n,-m]+AntE[-n,-m])},MetricOn->All,ContractMetrics->True];
+ExpandCetF=MakeRule[{CetF[i,-m],G[i,-m]+(1/2)*Prt*G[i,j]*G[n,-j]*(SymF[-n,-m]+AntF[-n,-m])},MetricOn->All,ContractMetrics->True];
+ExpandTetE=MakeRule[{TetE[-i,m],G[-i,m]-(1/2)*Prt*G[m,s]*G[-i,n]*(SymE[-s,-n]+AntE[-s,-n]-(1/2)*Prt*(SymE[-s,l]+AntE[-s,l])*(SymE[-l,-n]+AntE[-l,-n]))},MetricOn->All,ContractMetrics->True];
+ExpandTetF=MakeRule[{TetF[-i,m],G[-i,m]-(1/2)*Prt*G[m,s]*G[-i,n]*(SymF[-s,-n]+AntF[-s,-n]-(1/2)*Prt*(SymF[-s,l]+AntF[-s,l])*(SymF[-l,-n]+AntF[-l,-n]))},MetricOn->All,ContractMetrics->True];
+
+
+LineariseModel[InputExpr_]:=Module[{Expr=InputExpr,LinExpr},
+	Comment@"Linearizing...";	
+	Expr=Expr/.ExpandTE;
+	Expr=Expr/.ExpandCetE;
+	Expr=Expr/.ExpandCetF;
+	Expr=Expr/.ExpandTetE;
+	Expr=Expr/.ExpandTetF;
+	Expr//=Expand;
+	LinExpr=Coefficient[Expr,Prt];
+	Expr=Coefficient[Expr,Prt^2];
+	LinExpr//=ToCanonical;
+	LinExpr//=ContractMetric;
+	LinExpr//=ScreenDollarIndices;
+	LinExpr//=CollectTensors;
+	DisplayExpression@LinExpr;
+	Expr//=ToCanonical;
+	Expr//=ContractMetric;
+	Expr//=ScreenDollarIndices;
+	Expr//=CollectTensors;
+	DisplayExpression@Expr;
+Expr];
+
+Comment@"Normalisations.";
+Expr=CetE[i,-m]TetE[-i,n];
+Expr//LineariseModel;
+
+Comment@"Normalisations.";
+Expr=CetF[i,-m]TetF[-i,n];
+Expr//LineariseModel;
+
+Comment@"Normalisations.";
+Expr=CetE[i,-m]TetE[-j,m];
+Expr//LineariseModel;
+
+Comment@"Normalisations.";
+Expr=CetF[i,-m]TetF[-j,m];
+Expr//LineariseModel;
+
+Comment@"The kinetic term.";
+Expr=C1*TE[-m,-n,-s]*TE[m,n,s]+C2*TE[-m,-n,-s]*TE[n,m,s]-(2*C1+C2)*TE[-m,-n,m]*TE[s,n,-s];
+Expr//=LineariseModel;
+FirstExpr=Expr;
+
+Comment@"The mass term.";
+Expr=TetE[-a,b]*TetE[-c,d](Gam*G[a,c]CetF[z,-b]CetF[-z,-d]-JBet0*CetF[a,-d]CetF[c,-b]);
+Expr//=LineariseModel;
+SecondExpr=Expr;
+(**)
+Comment@"The volume four-forms.";
+Expr=-2*epsilonG[-a,-b,-c,-d]*epsilonG[e,f,g,h]*(
+	JBet0*CetE[a,-e]*CetE[b,-f]*CetE[c,-g]*CetE[d,-h]
+	+JBet1*CetE[a,-e]*CetE[b,-f]*CetE[c,-g]*CetF[d,-h]
+	+JBet2*CetE[a,-e]*CetE[b,-f]*CetF[c,-g]*CetF[d,-h]
+	+JBet3*CetE[a,-e]*CetF[b,-f]*CetF[c,-g]*CetF[d,-h]
+	+JBet4*CetF[a,-e]*CetF[b,-f]*CetF[c,-g]*CetF[d,-h]
+);
+(*
+NoCRules=First@Solve[4JBet0+3JBet1+2JBet2+JBet3==0&&JBet1+2JBet2+3JBet3+4JBet4==0,{JBet3,JBet4}];
+DisplayExpression@NoCRules;
+Expr=Expr/.NoCRules;
+*)
+Expr//=LineariseModel;
+ThirdExpr=Expr;
+(**)
+(*+SecondExpr*)
+Expr=FirstExpr+ThirdExpr;
+Expr//=ToCanonical;
+Expr//=ContractMetric;
+Expr//=ScreenDollarIndices;
+Expr//=CollectTensors;
+Expr//DisplayExpression;
+
+Code[Expr,      
+	ParticleSpectrum[
+		Expr,
+		TheoryName->"NLC",	
+		Method->"Hard",
+		MaxLaurentDepth->3
+	];
+];
+
+(*
 Comment@"We will define some coupling constants to parameterise the sector which is quadratic in the torsion:";
 Code[
 	DefConstantSymbol[C1,PrintAs->"\!\(\*SubscriptBox[\(\[ScriptC]\), \(1\)]\)"];
@@ -146,3 +262,4 @@ TryCombo[InputRule_]:=Module[{CaseRule,TheNonlinearLagrangian},
 	CaseNumber+=1;
 ];
 TryCombo/@Eqs;
+*)
