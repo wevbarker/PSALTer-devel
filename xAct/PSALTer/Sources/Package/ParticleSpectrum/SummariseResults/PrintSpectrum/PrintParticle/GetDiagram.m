681582f0349f3a36e2be4543cfef625dabbb272a
William Barker <wb263@cam.ac.uk>
2024-05-16 16:17:30 +0100

    with Tikz

--- xAct/PSALTer/Sources/Package/ParticleSpectrum/SummariseResults/PrintSpectrum/PrintParticle/GetDiagram.m
+++ xAct/PSALTer/Sources/Package/ParticleSpectrum/SummariseResults/PrintSpectrum/PrintParticle/GetDiagram.m
@@ -5,4 +5,5 @@
 GetDiagram[FileName_]:=Module[{TemporaryFileName,Expr},
 	TemporaryFileName=FileNameJoin@{$PSALTerInstallDirectory,"Sources/Package/ParticleSpectrum/SummariseResults/PrintSpectrum/PrintParticle",FileName};
 	Expr=TemporaryFileName~Import~{"PDF","PageGraphics"};
+	Expr//=First;
 Expr];
