c0452a56cb35863dadb902937f80b7c95489fa10
William Barker <wb263@cam.ac.uk>
2024-05-19 02:20:56 +0100

    changes to README

--- xAct/PSALTer/Documentation/English/Documentation/HPC.m
+++ xAct/PSALTer/Documentation/English/Documentation/HPC.m
@@ -2,12 +2,14 @@
 (*  HPC  *)
 (*=======*)
 
+(*
 $NodeCPU=16;
 Comment@{"The variable $NodeCPU is set to ",ToString@$NodeCPU};
 Comment@{"The variable $ProcessorCount is set to ",ToString@$ProcessorCount};
 Unprotect@$ProcessorCount;
 $ProcessorCount=$NodeCPU;
 Comment@{"The variable $ProcessorCount is set to ",ToString@$ProcessorCount};
+*)
 (*
 Comment@{"The variable $KernelCount is set to ",ToString@$KernelCount};
 Comment@{"Now we execute LaunchKernels[]"};
