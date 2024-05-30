0c9eca93209136de890a92a977c8a166d73e40f2
William Barker <wb263@cam.ac.uk>
2024-05-30 21:14:09 +0100

    push to system-orange

--- xAct/PSALTer/Documentation/English/Documentation.m
+++ xAct/PSALTer/Documentation/English/Documentation.m
@@ -15,14 +15,15 @@ Get@FileNameJoin@{$ThisDirectory,"Documentation","HowToUse.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","LoadingPSALTer.m"};
 
 Chapter@"Science applications";
-(*xAct`PSALTer`Private`$Disabled=True;*)
-xAct`PSALTer`Private`$DiagnosticMode=True;
+xAct`PSALTer`Private`$Disabled=True;
+(*xAct`PSALTer`Private`$DiagnosticMode=True;*)
 $ReadOnly=False;
 Get@FileNameJoin@{$ThisDirectory,"Documentation","ScalarTheory.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","VectorTheory.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","AntisymmetricTensorTheory.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","TensorTheory.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","ScalarTensorTheory.m"};
+xAct`PSALTer`Private`$Disabled=False;
 Get@FileNameJoin@{$ThisDirectory,"Documentation","PoincareGaugeTheory.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","MetricAffineGravity.m"};
 $ReadOnly=False;
