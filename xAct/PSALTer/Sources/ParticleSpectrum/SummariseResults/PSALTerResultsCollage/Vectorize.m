d6acf695d585d44ddf195634347019defd45f4aa
William Barker <wb263@cam.ac.uk>
2024-05-31 13:58:20 +0100

    removing covariant expression of sources

--- xAct/PSALTer/Sources/ParticleSpectrum/SummariseResults/PSALTerResultsCollage/Vectorize.m
+++ xAct/PSALTer/Sources/ParticleSpectrum/SummariseResults/PSALTerResultsCollage/Vectorize.m
@@ -6,6 +6,6 @@ Vectorize[InputExpr_]:=Module[{TemporaryFileName,Expr},
 	TemporaryFileNamePDF=FileNameJoin@{$WorkingDirectory,"tmp","Vectorized"<>".pdf"};
 	TemporaryFileNameEPS=FileNameJoin@{$WorkingDirectory,"tmp","Vectorized"<>".eps"};
 	Export[TemporaryFileNamePDF,InputExpr,AllowRasterization->False];
-	Run@("inkscape "<>TemporaryFileNamePDF<>" --export-filename="<>TemporaryFileNameEPS);
+	Run@("inkscape "<>TemporaryFileNamePDF<>" --export-eps="<>TemporaryFileNameEPS);
 	Expr=TemporaryFileNameEPS~Import~"Graphics";
 Expr];
