a403d32b729faf7540147da6d789170fc9e4f10e
William Barker <wb263@cam.ac.uk>
2024-05-31 18:52:26 +0100

    with fixed run command

--- xAct/PSALTer/Sources/ParticleSpectrum/SummariseResults/PSALTerResultsCollage/Vectorize.m
+++ xAct/PSALTer/Sources/ParticleSpectrum/SummariseResults/PSALTerResultsCollage/Vectorize.m
@@ -7,9 +7,11 @@ Vectorize[InputExpr_]:=Module[{TemporaryFileNameEPS,TemporaryFileNamePDF,Tempora
 	TemporaryFileNameEPS=FileNameJoin@{$WorkingDirectory,"tmp","Vectorized"<>".eps"};
 	TemporaryFileNameTXT=FileNameJoin@{$WorkingDirectory,"tmp","Vectorized"<>".txt"};
 	Export[TemporaryFileNamePDF,InputExpr,AllowRasterization->False];
+(*
 	Run@("\"C:\\Program Files\\Inkscape\\inkscape.com\" "<>TemporaryFileNamePDF<>" --export-eps="<>TemporaryFileNameEPS);
 	(*Run@("where /r \"C:\\Program Files\" inkscape.com > \""<>TemporaryFileNameTXT<>"\" & set /p myvar= < \""<>TemporaryFileNameTXT<>"\" & \"%myvar%\" "<>TemporaryFileNamePDF<>" --export-eps="<>TemporaryFileNameEPS);*)
 	Run@("inkscape "<>TemporaryFileNamePDF<>" --export-eps="<>TemporaryFileNameEPS);
+*)
 	(*Expr=TemporaryFileNameEPS~Import~"Graphics";*)
 	Expr=TemporaryFileNamePDF~Import~"Graphics";
 Expr];
