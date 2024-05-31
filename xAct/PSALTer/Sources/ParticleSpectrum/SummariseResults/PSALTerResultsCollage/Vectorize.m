537cfa8121cb468c381c278eeb4a71a8229dd4ed
William Barker <wb263@cam.ac.uk>
2024-05-31 19:13:42 +0100

    with fixed run command

--- xAct/PSALTer/Sources/ParticleSpectrum/SummariseResults/PSALTerResultsCollage/Vectorize.m
+++ xAct/PSALTer/Sources/ParticleSpectrum/SummariseResults/PSALTerResultsCollage/Vectorize.m
@@ -5,13 +5,13 @@
 Vectorize[InputExpr_]:=Module[{TemporaryFileNameEPS,TemporaryFileNamePDF,TemporaryFileNameTXT,Expr},
 	TemporaryFileNamePDF=FileNameJoin@{$WorkingDirectory,"tmp","Vectorized"<>".pdf"};
 	TemporaryFileNameEPS=FileNameJoin@{$WorkingDirectory,"tmp","Vectorized"<>".eps"};
-	TemporaryFileNameTXT=FileNameJoin@{$WorkingDirectory,"tmp","Vectorized"<>".txt"};
 	Export[TemporaryFileNamePDF,InputExpr,AllowRasterization->False];
-(*
-	Run@("\"C:\\Program Files\\Inkscape\\inkscape.com\" "<>TemporaryFileNamePDF<>" --export-eps="<>TemporaryFileNameEPS);
+	Which[($OperatingSystem==="Unix")||($OperatingSystem==="MacOSX"),
+		Run@("inkscape "<>TemporaryFileNamePDF<>" --export-eps="<>TemporaryFileNameEPS);
+		Expr=TemporaryFileNameEPS~Import~"Graphics";
+	$OperatingSystem==="Windows",
+		Expr=TemporaryFileNamePDF~Import~"PageGraphics";
+		Expr//=First;
 	(*Run@("where /r \"C:\\Program Files\" inkscape.com > \""<>TemporaryFileNameTXT<>"\" & set /p myvar= < \""<>TemporaryFileNameTXT<>"\" & \"%myvar%\" "<>TemporaryFileNamePDF<>" --export-eps="<>TemporaryFileNameEPS);*)
-	Run@("inkscape "<>TemporaryFileNamePDF<>" --export-eps="<>TemporaryFileNameEPS);
-*)
-	(*Expr=TemporaryFileNameEPS~Import~"Graphics";*)
-	Expr=TemporaryFileNamePDF~Import~"PageGraphics";
+	];
 Expr];
