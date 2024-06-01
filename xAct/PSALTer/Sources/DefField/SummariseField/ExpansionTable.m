51a1a97b74d33e937de92c0d7c48bed234f1bb67
William Barker <wb263@cam.ac.uk>
2024-06-01 22:46:53 +0100

    transfer to system-orange

--- xAct/PSALTer/Sources/DefField/SummariseField/ExpansionTable.m
+++ xAct/PSALTer/Sources/DefField/SummariseField/ExpansionTable.m
@@ -16,6 +16,5 @@ ExpansionTable[FieldSpinParityTensors_,
 		),
 			ItemSize->{Full,Full,{1,3}->70},
 			Dividers->Center,
-			Alignment->{Left,Center},
-			Background->DetailColor],Background->DetailColor,FrameStyle->Directive[DetailColor,Thickness[4]],RoundingRadius->$FrameRoundingRadius];
+			Alignment->{Left,Center}],Background->DetailColor,FrameStyle->Directive[DetailColor,Thickness[4]],RoundingRadius->$FrameRoundingRadius];
 FinalGrid];
