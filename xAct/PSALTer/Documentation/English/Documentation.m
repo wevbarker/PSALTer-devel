62288a76b01e68af146a46ca21a5662f42a8fcf1
William Barker <wb263@cam.ac.uk>
2024-06-01 15:04:45 +0100

    transfer to system-orange

--- xAct/PSALTer/Documentation/English/Documentation.m
+++ xAct/PSALTer/Documentation/English/Documentation.m
@@ -17,7 +17,7 @@ Get@FileNameJoin@{$ThisDirectory,"Documentation","LoadingPSALTer.m"};
 Chapter@"Science applications";
 (*xAct`PSALTer`Private`$Disabled=True;*)
 (*xAct`PSALTer`Private`$DiagnosticMode=True;*)
-$ReadOnly=False;
+$ReadOnly=True;
 Get@FileNameJoin@{$ThisDirectory,"Documentation","ScalarTheory.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","VectorTheory.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","AntisymmetricTensorTheory.m"};
