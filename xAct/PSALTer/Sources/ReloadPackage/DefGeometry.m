e8e38743d5b588276ac576ea6fe3c7eab930deb4
William Barker <wb263@cam.ac.uk>
2024-06-02 13:38:54 +0100

    transfer to system-orange

--- xAct/PSALTer/Sources/ReloadPackage/DefGeometry.m
+++ xAct/PSALTer/Sources/ReloadPackage/DefGeometry.m
@@ -45,7 +45,7 @@ DefTensor[Eps[-a,-b,-c],M4,Antisymmetric[{-a,-b,-c}],
 	PrintAs->"\!\(\*SuperscriptBox[\(\[Epsilon]\), \(\[DoubleVerticalBar]\)]\)"];
 DefConstantSymbol[Def,PrintAs->"\[ScriptK]"];
 DefConstantSymbol[xAct`PSALTer`Private`DefSquared,PrintAs->"\[ScriptX]"];
-DefConstantSymbol[Action,PrintAs->"\!\(\*SuperscriptBox[\(\[ScriptCapitalS]\), \(F\)]\)"];
+DefConstantSymbol[Action,PrintAs->"\!\(\*SubscriptBox[\(\[ScriptCapitalS]\), \(F\)]\)"];
 	(*PrintAs->"\[ScriptCapitalS]"];*)
 DefConstantSymbol[xAct`PSALTer`Private`TCoordinate,PrintAs->"\[ScriptT]"];
 DefConstantSymbol[xAct`PSALTer`Private`XCoordinate,PrintAs->"\[ScriptX]"];
@@ -68,38 +68,6 @@ AllComponentValues[P[{a,cartesian}],{En,0,0,Mo}];
 AllComponentValues[P[{-a,-cartesian}],{En,0,0,-Mo}];
 AllComponentValues[G[{a,cartesian},{b,cartesian}],DiagonalMatrix@{1,-1,-1,-1}];
 AllComponentValues[G[{-a,-cartesian},{-b,-cartesian}],DiagonalMatrix@{1,-1,-1,-1}];
-Off@ValidateSymbol::used;
-Begin["xAct`PSALTer`Private`"];
-(*
-DefConstantSymbol[
-	Symbol["X"<>ToString@#],
-	PrintAs->"\!\(\*SubscriptBox[\(\[ScriptCapitalX]\), \("
-		<>ColorString[ToString@#,RGBColor[1.,0.,1.]]<>"\)]\)",
-	Dagger->Complex
-]&/@Table[i,{i,100}];
-DefConstantSymbol[
-	Symbol["UniqueCoefficient"<>ToString@#],
-	PrintAs->"\!\(\*SubscriptBox[\(\[ScriptCapitalC]\), \("
-		<>ColorString[ToString@#,RGBColor[1.,0.,1.]]<>"\)]\)",
-	Dagger->Complex
-]&/@Table[i,{i,10000}];
-DefConstantSymbol[
-	Symbol["l"<>ToString@#],
-	PrintAs->"\!\(\*SubscriptBox[\(\[ScriptL]\), \("
-		<>ColorString[ToString@#,RGBColor[1.,0.,1.]]<>"\)]\)",
-	Dagger->Complex
-]&/@Table[i,{i,100}];
-Table[DefConstantSymbol[
-	Quiet@Symbol["r"<>ToString@i<>ToString@j],
-	PrintAs->"\!\(\*SubscriptBox[\(\[ScriptR]\), \("<>ColorString[ToString@i<>ToString@j,RGBColor[1.,0.,1.]]<>"\)]\)",
-	Dagger->Complex],{i,20},{j,20}];
-Table[DefConstantSymbol[
-	Quiet@Symbol["i"<>ToString@i<>ToString@j],
-	PrintAs->"\!\(\*SubscriptBox[\(\[ScriptI]\), \("<>ColorString[ToString@i<>ToString@j,RGBColor[1.,0.,1.]]<>"\)]\)",
-	Dagger->Complex],{i,20},{j,20}];
-*)
-End[];
-On@ValidateSymbol::used;
 DefConstantSymbol[PoleResidue,PrintAs->"\[Lambda]"];
 $CVVerbose=True;
 $DefInfoQ=True;
