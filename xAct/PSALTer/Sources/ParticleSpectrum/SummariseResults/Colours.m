b4984860d73aa66858febf87fc63438cb33e89ae
William Barker <wb263@cam.ac.uk>
2024-06-04 18:25:04 +0100

    transfer to system orange

--- xAct/PSALTer/Sources/ParticleSpectrum/SummariseResults/Colours.m
+++ xAct/PSALTer/Sources/ParticleSpectrum/SummariseResults/Colours.m
@@ -3,7 +3,7 @@
 (*===========*)
 
 $FrameRoundingRadius=10;
-$PanelColor=Cyan;
+$PanelColor=LightCyan;
 $DetailColor=LightGreen;
 $ParityEvenColor=RGBColor[255/255,204/255,204/255];
 $ParityOddColor=RGBColor[204/255,229/255,255/255];
