5c56aee0fc0272c4e3b6b211bbac32e46c922754
William Barker <wb263@cam.ac.uk>
2024-06-09 17:07:10 +0100

    update of sources

--- xAct/PSALTer/Documentation/English/Documentation/PoincareGaugeTheory.m
+++ xAct/PSALTer/Documentation/English/Documentation/PoincareGaugeTheory.m
@@ -91,5 +91,7 @@ Get@FileNameJoin@{$ThisDirectory,"Documentation","PoincareGaugeTheory",
 *)
 Get@FileNameJoin@{$ThisDirectory,"Documentation","PoincareGaugeTheory",
 					"GeneralPGT.m"};
+(*
 Get@FileNameJoin@{$ThisDirectory,"Documentation","PoincareGaugeTheory",
 					"LinHobsonLasenbyTheories.m"};
+*)
