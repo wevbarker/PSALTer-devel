5452c94e6db3bf08f205b6df149b9030ee7cee83
William Barker <wb263@cam.ac.uk>
2024-06-14 21:10:01 +0100

    with installers

--- InstallMacOSGitGlobal.sh
+++ InstallMacOSGitGlobal.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 cd ~/
+ls /Library/Mathematica/Applications/xAct
 sudo rm -rf /Library/Mathematica/Applications/xAct/PSALTer
+ls /Library/Mathematica/Applications/xAct
+ls ~/Library/Mathematica/Applications/xAct
 rm -rf ~/Library/Mathematica/Applications/xAct/PSALTer
+ls ~/Library/Mathematica/Applications/xAct
 rm -rf ~/PSALTer-devel
 
 git clone https://github.com/wevbarker/PSALTer-devel
 cd PSALTer-devel/xAct
 sudo cp -r PSALTer /Library/Mathematica/Applications/xAct/
+ls /Library/Mathematica/Applications/xAct
+cd ~/Documents/PSALTer-devel
 
 exit 0
--- InstallMacOSZip.sh
+++ InstallMacOSZip.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 cd ~/
+ls /Library/Mathematica/Applications/xAct
 sudo rm -rf /Library/Mathematica/Applications/xAct/PSALTer
+ls /Library/Mathematica/Applications/xAct
+ls ~/Library/Mathematica/Applications/xAct
 rm -rf ~/Library/Mathematica/Applications/xAct/PSALTer
+ls ~/Library/Mathematica/Applications/xAct
 rm -rf ~/PSALTer-devel
 
 unzip ~/Downloads/wevbarker-PSALTer-devel-* ~/
 mv ~/wevbarker-PSALTer-devel-* ~/PSALTer-devel
 cd PSALTer-devel/xAct
 sudo cp -r PSALTer /Library/Mathematica/Applications/xAct/
+ls /Library/Mathematica/Applications/xAct
+cd ~/Documents/PSALTer-devel
 
 exit 0
