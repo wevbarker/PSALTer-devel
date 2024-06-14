8476ab62a42e6519561649dcc1e55f1f3145f47e
William Barker <wb263@cam.ac.uk>
2024-06-14 20:26:27 +0100

    with installers

--- InstallWindowsGitGlobal.cmd
+++ InstallWindowsGitGlobal.cmd
@@ -1,8 +1,14 @@
 cd "C:\Users\user"
+ls "C:\Program Files\Wolfram Research\Mathematica\14.0\AddOns\Applications\xAct"
 rm -rf "C:\Program Files\Wolfram Research\Mathematica\14.0\AddOns\Applications\xAct\PSALTer"
+ls "C:\Program Files\Wolfram Research\Mathematica\14.0\AddOns\Applications\xAct"
+ls "C:\Users\user\AppData\Roaming\Mathematica\Applications\xAct"
 rm -rf "C:\Users\user\AppData\Roaming\Mathematica\Applications\xAct\PSALTer"
+ls "C:\Users\user\AppData\Roaming\Mathematica\Applications\xAct"
 rm -rf "C:\Users\user\PSALTer-devel"
 
 git clone https://github.com/wevbarker/PSALTer-devel
-cd PSALTer\xAct
+cd PSALTer-devel\xAct
 xcopy /e /k /h /i PSALTer "C:\Program Files\Wolfram Research\Mathematica\14.0\AddOns\Applications\xAct"
+ls "C:\Program Files\Wolfram Research\Mathematica\14.0\AddOns\Applications\xAct"
+cd "C:\Users\user\Documents\PSALTer-devel"
