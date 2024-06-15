2523a74f2203d3adf191b39b742ced9e27ce4f94
William Barker <wb263@cam.ac.uk>
2024-06-15 23:16:24 +0100

    with installers

--- xAct/PSALTer/Documentation/English/Documentation.m
+++ xAct/PSALTer/Documentation/English/Documentation.m
@@ -17,7 +17,7 @@ Get@FileNameJoin@{$ThisDirectory,"Documentation","LoadingPSALTer.m"};
 Chapter@"Science applications";
 xAct`PSALTer`Private`$Disabled=False;
 xAct`PSALTer`Private`$DiagnosticMode=False;
-$ReadOnly=False;
+$ReadOnly=True;
 xAct`PSALTer`Private`$NoExport=False;
 Get@FileNameJoin@{$ThisDirectory,"Documentation","ScalarTheory.m"};
 Get@FileNameJoin@{$ThisDirectory,"Documentation","VectorTheory.m"};
