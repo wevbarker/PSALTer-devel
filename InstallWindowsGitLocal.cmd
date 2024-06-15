bb932f0774794fc3c0add23e018029a76199d3d2
William Barker <wb263@cam.ac.uk>
2024-06-15 15:48:20 +0100

    with installers

--- InstallWindowsGitLocal.cmd
+++ InstallWindowsGitLocal.cmd
@@ -9,6 +9,7 @@ rm -rf "C:\Users\user\PSALTer-devel"
 
 git clone https://github.com/wevbarker/PSALTer-devel
 cd PSALTer-devel
+git checkout README
 xcopy /e /k /h /i xAct\ "C:\Users\user\AppData\Roaming\Mathematica\Applications\xAct\"
 ls "C:\Users\user\AppData\Roaming\Mathematica\Applications\xAct"
 cd "C:\Users\user\Documents\PSALTer-devel"
