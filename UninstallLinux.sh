b0cc65ff6b15c19894c78ebb188719c604cbfbdf
William Barker <wb263@cam.ac.uk>
2024-06-17 01:09:27 +0100

    push for merge with master

---
+++ UninstallLinux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ls ~/.Mathematica/Applications/xAct
+rm -rf ~/.Mathematica/Applications/xAct/PSALTer
+ls ~/.Mathematica/Applications/xAct
+
+ls /usr/share/Mathematica/Applications/xAct
+sudo rm -rf /usr/share/Mathematica/Applications/xAct/PSALTer
+ls /usr/share/Mathematica/Applications/xAct
